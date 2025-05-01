7e119edc43e4dbfe4948ddef337abf8f0a171477
JeffreyXu <shuidi_dev@pingansec.com>
2025-05-01 23:55:55 +0800

    修改Tool[search_*], 只支持对单个province、city、district的查询； 增加province city district的参数归一化处理 增加company_status参数的验证及归一化处理 修改对应的apidoc及README

--- README.md
+++ README.md
@@ -9,63 +9,86 @@
 - `query_company_data`:根据自然语言问题回答与企业主体相关的统计问题，并且可以根据要求可视化展示为地图、折线图、柱状图、饼图等
   - 参数:
       - `question` (string): 问题描述，例如：例如统计全国各省2024年成立的企业，并以地图可视化展示 
+  
 - `search_established_companies`:查询并统计某时间段成立的企业
   - 参数:
-      - `province` (string): 省份最多5个，多个省份用英文逗号隔开,当查询全国时，不设province参数。
-      - `city` (string): 城市,多个城市用英文逗号隔开,city参数必须设为地级市。
-      - `district` (string): 区县,多个区县用英文逗号隔开。
-      - `establishDate` (string) : 成立期限，格式如2023-01-12@2023-11-11 表示2012年1月12日到2023年11月11日之间成立的企业,2023-01-12@表示2023年1月12号及之后成立的企业,@2023-11-11表示23年11月11日之前成立的公司。
+      - `province` (string): 省份,例如上海,新疆,江苏,为None时表示查询全国的企业。
+      - `city` (string): 城市,地级市，例如杭州，苏州。
+      - `district` (string): 区县,区县名称需完整,包含市、县、区,例如昆山市，浦东新区。
+      - `establish_date` (string) : 成立期限，格式如yyyy-mm-dd@yyyy-mm-dd,
+                                             2023-01-12@表示2023年1月12号及之后成立的企业, 
+                                             @2023-11-11表示23年11月11日之前成立的公司, 
+                                             为None时表示任意时间成立的企业。
+    
 - `search_established_selfemployed`:查询并统计某时间段成立的个体户
   - 参数:
-      - `province` (string): 省份最多5个，多个省份用英文逗号隔开,当查询全国时，不设province参数。
-      - `city` (string): 城市,多个城市用英文逗号隔开,city参数必须设为地级市。
-      - `district` (string): 区县,多个区县用英文逗号隔开。
-      - `establishDate` (string) : 成立期限，格式如2023-01-12@2023-11-11 表示2012年1月12日到2023年11月11日之间成立的企业,2023-01-12@表示2023年1月12号及之后成立的企业,@2023-11-11表示23年11月11日之前成立的公司。
+     - `province` (string): 省份,例如上海,新疆,江苏,为None时表示查询全国的企业。
+      - `city` (string): 城市,地级市，例如杭州，苏州。
+      - `district` (string): 区县,区县名称需完整,包含市、县、区,例如昆山市，浦东新区。
+      - `establish_date` (string) : 成立期限，格式如yyyy-mm-dd@yyyy-mm-dd,
+                                             2023-01-12@表示2023年1月12号及之后成立的企业, 
+                                             @2023-11-11表示23年11月11日之前成立的公司, 
+                                             为None时表示任意时间成立的企业。
+    
 - `search_companies`:根据地区及企业状态查询并统计企业
   - 参数:
-      - `province` (string): 省份最多5个，多个省份用英文逗号隔开,当查询全国时，不设province参数。
-      - `city` (string): 城市,多个城市用英文逗号隔开,city参数必须设为地级市。
-      - `district` (string): 区县,多个区县用英文逗号隔开。
-      - `establishDate` (string) : 成立期限，格式如2023-01-12@2023-11-11 表示2012年1月12日到2023年11月11日之间成立的企业,2023-01-12@表示2023年1月12号及之后成立的企业,@2023-11-11表示23年11月11日之前成立的公司。 
-      - `company_status` (string) : 企业状态可以设为"存续,在业"、"吊销"、"注销"、"迁入"、"迁出"、"撤销"、"清算"、"停止"、"其他"
+      - `province` (string): 省份,例如上海,新疆,江苏,为None时表示查询全国的企业。
+      - `city` (string): 城市,地级市，例如杭州，苏州。
+      - `district` (string): 区县,区县名称需完整,包含市、县、区,例如昆山市，浦东新区。
+      - `company_status` (string) : 企业状态可以设为"正常"、"异常"、"在营"、"存续"、"在业"、"吊销"、"注销"、"迁入"、"迁出"、"撤销"、"清算"、"停业"、"其他",
+                                    企业状态正常，包括了在营、存续、在业、迁入、迁出的企业,
+                                    企业状态异常，包括了吊销、注销企业。
+    
 - `search_selfemployed`:根据地区及企业状态查询并统计个体户
   - 参数:
-      - `province` (string): 省份最多5个，多个省份用英文逗号隔开,当查询全国时，不设province参数。
-      - `city` (string): 城市,多个城市用英文逗号隔开,city参数必须设为地级市。
-      - `district` (string): 区县,多个区县用英文逗号隔开。
-      - `establishDate` (string) : 成立期限，格式如2023-01-12@2023-11-11 表示2012年1月12日到2023年11月11日之间成立的企业,2023-01-12@表示2023年1月12号及之后成立的企业,@2023-11-11表示23年11月11日之前成立的公司。 
-      - `company_status` (string) : 状态可以设为"存续,在业"、"吊销"、"注销"、"迁入"、"迁出"、"撤销"、"清算"、"停止"、"其他"
+      - `province` (string): 省份,例如上海,新疆,江苏,为None时表示查询全国的企业。
+      - `city` (string): 城市,地级市，例如杭州，苏州。
+      - `district` (string): 区县,区县名称需完整,包含市、县、区,例如昆山市，浦东新区。
+      - `company_status` (string) : 企业状态可以设为"正常"、"异常"、"在营"、"存续"、"在业"、"吊销"、"注销"、"迁入"、"迁出"、"撤销"、"清算"、"停业"、"其他",
+                                    企业状态正常，包括了在营、存续、在业、迁入、迁出的企业,
+                                    企业状态异常，包括了吊销、注销企业。
+    
 - `get_company_info`:查询企业基本信息（照面信息）
   - 参数:
       - `company_name` (string): 企业名称
+    
 - `get_company_partner`:查询企业股东信息
   - 参数:
       - `company_name` (string): 企业名称
+    
 - `get_stie_score`:评估企业科创能力
   - 参数:
       - `company_name` (string): 企业名称
+    
 - `search_company_risk`:查询企业风险信息，包括企业自身风险、周边风险、预警信息等
   - 参数:
       - `company_name` (string): 企业名称
+    
 - `get_company_honor`:查询企业荣誉
   - 参数:
       - `company_name` (string): 企业名称
+    
 - `get_company_contact`:查询企业联系方式
   - 参数:
       - `company_name` (string): 企业名称
+    
 - `get_company_investment`:查询企业对外投资信息
   - 参数:
       - `company_name` (string): 企业名称
+    
 - `get_company_cert`:查询企业资质证书
   - 参数:
       - `company_name` (string): 企业名称
+    
 - `get_person_related_company`:根据企业名称和人名查询企业人员的关联企业,包括其担任法人、股东、董监高的公司
   - 参数:
       - `company_name` (string): 企业名称
       - `person_name` (string): 人名
+    
 - `get_company_controller`:查询企业实控人
   - 参数:
       - `company_name` (string): 企业名称
+    
 - `get_company_benificalowner`:查询企业受益所有人
   - 参数:
       - `company_name` (string): 企业名称
--- src/mcp_server.py
+++ src/mcp_server.py
@@ -1,21 +1,25 @@
-import asyncio
-
 from loguru import logger
 from mcp.server import FastMCP
 
 from api_tool import ApiAdapter, SearchApiAdapter
+from normalizer import Area, DateRange, CompanyStatus
 
 mcp = FastMCP(title='Shuidi DataMcpServer', version='1.0')
 
 @mcp.tool()
-async def search_established_companies(province=None, city=None, district=None, establish_date=None) -> dict:
-    '''
+async def search_established_companies(province=None, city=None, district=None, establish_date=None):
+    """
     输入省份、城市、成立日期查询并统计某时间段成立的企业
     回答问题时结果中请先说明查询到的企业数，并以列表的形式列出查询到的前10条企业
-    :param province  String  省份最多5个，多个省份用英文逗号隔开,当查询全国时，不设province参数。province参数取两个字，不要有省或市
-    :param city String  城市,多个城市用英文逗号隔开,city参数必须设为地级市
-    :param district String  区县,多个区县用英文逗号隔开
-    :param establishDate   String  成立期限，格式如2023-01-12@2023-11-11 表示2012年1月12日到2023年11月11日之间成立的企业，2023-01-12@2023-01-12 表示2012年1月12日成立的企业，2023-01-12@表示2023年1月12号及之后成立的企业， @2023-11-11表示23年11月11日之前成立的公司。 2023-01-01@2023-12-31表示23年成立的企业。
+    :param province String 省份,例如上海,新疆,江苏,为None时表示查询全国的企业
+    :param city String 城市,地级市，例如杭州，苏州
+    :param district String 区县,区县名称需完整,包含市、县、区,例如昆山市，浦东新区
+    :param establish_date String 成立期限，格式如yyyy-mm-dd@yyyy-mm-dd
+                                          2023-01-12@2023-11-11 表示2012年1月12日到2023年11月11日之间成立的企业,
+                                          2023-01-12@2023-01-12 表示2012年1月12日成立的企业,
+                                          2023-01-12@表示2023年1月12号及之后成立的企业,
+                                          @2023-11-11表示23年11月11日之前成立的公司,
+                                          为None时表示任意时间成立的企业。
 
     :return
     status	String	状态码0代表成功
@@ -27,25 +31,32 @@ async def search_established_companies(province=None, city=None, district=None, 
     capital String 注册资本
     legalPerson String 法人
     creditNo String 统一信用码
-    '''
+    """
 
     try:
-        params = {'province': province, 'city':city, 'district':district, 'establishDate':establish_date, 'company_type':'有限责任公司,股份有限公司,股份合作公司,国有企业,央企,集体所有制,全民所有制,独资企业,有限合伙,普通合伙,外商投资企业,港、澳、台商投资企业,联营企业,私营企业'}
+        area = Area(province=province, city=city, district=district)
+        establish_date = DateRange(date_range=establish_date).date_range
+        params = {'province': area.province, 'city': area.city, 'district': area.district, 'establishDate':establish_date, 'company_type':'有限责任公司,股份有限公司,股份合作公司,国有企业,央企,集体所有制,全民所有制,独资企业,有限合伙,普通合伙,外商投资企业,港、澳、台商投资企业,联营企业,私营企业'}
         return await SearchApiAdapter().invoke(params)
     except Exception as e:
         logger.error(e)
-        return "查询统计企业失败！"
+        return f"查询统计企业失败！{e}"
 
 
 @mcp.tool()
-async def search_established_selfemployed(province=None, city=None, district=None, establish_date=None) -> dict:
-    '''
+async def search_established_selfemployed(province=None, city=None, district=None, establish_date=None):
+    """
     输入省份、城市、成立日期查询并统计某时间段成立的个体工商户
     回答问题时结果中请先说明查询到的记录数，并以列表的形式列出查询到的前10条个体工商户
-    :param province  String  省份最多5个，多个省份用英文逗号隔开,当查询全国时，不设province参数。province参数取两个字，不要有省或市
-    :param city    String  城市,多个城市用英文逗号隔开,city参数必须设为地级市
-    :param district String  区县,多个区县用英文逗号隔开,district参数必须完整，例如包含市、县或区
-    :param establishDate   String  成立期限，格式如2023-01-12@2023-11-11 表示2012年1月12日到2023年11月11日之间成立的企业，2023-01-12@2023-01-12 表示2012年1月12日成立的企业，2023-01-12@表示2023年1月12号及之后成立的企业， @2023-11-11表示23年11月11日之前成立的公司。 2023-01-01@2023-12-31表示23年成立的企业。
+    :param province String 省份,例如上海,新疆,江苏,为None时表示查询全国的企业
+    :param city String 城市,地级市，例如杭州，苏州
+    :param district String 区县,区县名称需完整,包含市、县、区,例如昆山市，浦东新区
+    :param establish_date String 成立期限，格式如yyyy-mm-dd@yyyy-mm-dd
+                                          2023-01-12@2023-11-11 表示2012年1月12日到2023年11月11日之间成立的企业,
+                                          2023-01-12@2023-01-12 表示2012年1月12日成立的企业,
+                                          2023-01-12@表示2023年1月12号及之后成立的企业,
+                                          @2023-11-11表示23年11月11日之前成立的公司,
+                                          为None时表示任意时间成立的企业。
 
     :return
     status	String	状态码0代表成功
@@ -57,25 +68,28 @@ async def search_established_selfemployed(province=None, city=None, district=Non
     capital String 注册资本
     legalPerson String 法人
     creditNo String 统一信用码
-    '''
+    """
 
     try:
-        params = {'province': province, 'city':city, 'district':district, 'establishDate':establish_date, 'company_type':'个体工商户'}
+        area = Area(province=province, city=city, district=district)
+        establish_date = DateRange(date_range=establish_date).date_range
+        params = {'province': area.province, 'city': area.city, 'district': area.district, 'establishDate':establish_date, 'company_type':'个体工商户'}
         return await SearchApiAdapter().invoke(params)
     except Exception as e:
         logger.error(e)
-        return "查询统计个体户失败！"
+        return f"查询统计个体户失败！{e}"
 
 @mcp.tool()
-async def search_companies(province=None, city=None, district=None, company_status=None) -> dict:
-    '''
+async def search_companies(province=None, city=None, district=None, company_status=None):
+    """
     输入省份、城市、企业状态查询并统计企业
     回答问题时结果中请先说明查询到的记录数，并以列表的形式列出查询到的前10条企业
-    :param province  String  省份最多5个，多个省份用英文逗号隔开,当查询全国时，不设province参数。province参数取两个字，不要有省或市
-    :param city    String  城市,多个城市用英文逗号隔开,city参数必须设为地级市
-    :param district String  区县,多个区县用英文逗号隔开,district参数必须完整，例如包含市、县或区
-    :param company_status   String  企业状态可以设为"存续,在业"、"吊销"、"注销"、"迁入"、"迁出"、"撤销"、"清算"、"停止"、"其他"
-
+    :param province String 省份,例如上海,新疆,江苏,为None时表示查询全国的企业
+    :param city String 城市,地级市，例如杭州，苏州
+    :param district String 区县,区县名称需完整,包含市、县、区,例如昆山市，浦东新区
+    :param company_status String 企业状态可以设为"正常"、"异常"、"在营"、"存续"、"在业"、"吊销"、"注销"、"迁入"、"迁出"、"撤销"、"清算"、"停业"、"其他"
+                                 企业状态正常，包括了在营、存续、在业、迁入、迁出的企业
+                                 企业状态异常，包括了吊销、注销企业
     :return
     status	String	状态码0代表成功
     data.num_found	String	返回的记录数
@@ -86,25 +100,28 @@ async def search_companies(province=None, city=None, district=None, company_stat
     capital String 注册资本
     legalPerson String 法人
     creditNo String 统一信用码
-    '''
+    """
 
     try:
-        params = {'province': province, 'city': city, 'district':district, 'company_status': company_status, 'company_type': '有限责任公司,股份有限公司,股份合作公司,国有企业,央企,集体所有制,全民所有制,独资企业,有限合伙,普通合伙,外商投资企业,港、澳、台商投资企业,联营企业,私营企业'}
+        area = Area(province=province, city=city, district=district)
+        company_status = CompanyStatus(status=company_status).status
+        params = {'province': area.province, 'city': area.city, 'district': area.district, 'company_status': company_status, 'company_type': '有限责任公司,股份有限公司,股份合作公司,国有企业,央企,集体所有制,全民所有制,独资企业,有限合伙,普通合伙,外商投资企业,港、澳、台商投资企业,联营企业,私营企业'}
         return await SearchApiAdapter().invoke(params)
     except Exception as e:
         logger.error(e)
-        return "查询统计企业失败！"
+        return f"查询统计企业失败！{e}"
 
 @mcp.tool()
-async def search_selfemployed(province=None, city=None, district=None, company_status=None) -> dict:
-    '''
+async def search_selfemployed(province=None, city=None, district=None, company_status=None):
+    """
     输入省份、城市、企业状态查询并统计个体户
     回答问题时结果中请先说明查询到的记录数，并以列表的形式列出查询到的前10条个体户
-    :param province  String  省份最多5个，多个省份用英文逗号隔开,当查询全国时，不设province参数。province参数取两个字，不要有省或市
-    :param city    String  城市,多个城市用英文逗号隔开,city参数必须设为地级市
-    :param district String  区县,多个区县用英文逗号隔开,district参数必须完整，例如包含市、县或区
-    :param company_status   String  企业状态可以设为"存续,在业"、"吊销"、"注销"、"迁入"、"迁出"、"撤销"、"清算"、"停止"、"其他"
-
+    :param province String 省份,例如上海,新疆,江苏,为None时表示查询全国的企业
+    :param city String 城市,地级市，例如杭州，苏州
+    :param district String 区县,区县名称需完整,包含市、县、区,例如昆山市，浦东新区
+    :param company_status String 企业状态可以设为"正常"、"异常"、"在营"、"存续"、"在业"、"吊销"、"注销"、"迁入"、"迁出"、"撤销"、"清算"、"停业"、"其他"
+                                 企业状态正常，包括了在营、存续、在业、迁入、迁出的企业
+                                 企业状态异常，包括了吊销、注销企业
     :return
     status	String	状态码0代表成功
     data.num_found	String	返回的记录数
@@ -115,16 +132,18 @@ async def search_selfemployed(province=None, city=None, district=None, company_s
     capital String 注册资本
     legalPerson String 法人
     creditNo String 统一信用码
-    '''
+    """
     try:
-        params = {'province': province, 'city': city, 'district':district, 'company_status': company_status, 'company_type': '个体工商户'}
+        area = Area(province=province, city=city, district=district)
+        company_status = CompanyStatus(status=company_status).status
+        params = {'province': area.province, 'city': area.city, 'district': area.district, 'company_status': company_status, 'company_type': '个体工商户'}
         return await SearchApiAdapter().invoke(params)
     except Exception as e:
         logger.error(e)
-        return "查询统计个体户失败!"
+        return f"查询统计个体户失败!{e}"
 
 @mcp.tool()
-async def get_company_info(company_name: str) -> dict:
+async def get_company_info(company_name: str):
     """
     根据公司名查询公司的基础信息（也称营业执照照面信息），包括统一信用码、企业注册码、企业类型、法定代表人、成立时间、注册资本、企业地址、营业期限、经营范围、登记机关、登记状态、核准时间
     :param company_name: 公司名称
@@ -145,15 +164,15 @@ async def get_company_info(company_name: str) -> dict:
         CreditNo	String	企业信用代码
     """
     try:
-        return await ApiAdapter('http://api.shuidi.cn/utn/ic/Base/V1').invoke({'keyword': company_name})
+        return await ApiAdapter('https://api.shuidi.cn/utn/ic/Base/V1').invoke({'keyword': company_name})
     except Exception as e:
         logger.error(e)
-        return f"获取企业[{company_name}]基础信息失败!"
+        return f"获取企业[{company_name}]基础信息失败!{e}"
 
 
 @mcp.tool()
-async def get_company_partner(company_name:str) -> dict:
-    '''
+async def get_company_partner(company_name:str):
+    """
     根据企业名称获取企业的股东信息
     :param company_name: 企业名称
     :return:
@@ -170,16 +189,16 @@ async def get_company_partner(company_name:str) -> dict:
         stockRealCapital String 实缴出资额
         stockCapital String 认缴出资额
         holderNum String 持股数
-    '''
+    """
     try:
         return await ApiAdapter('https://api.shuidi.cn/utn/ic/Partners/V2').invoke({'keyword': company_name})
     except Exception as e:
         logger.error(e)
-        return f"获取企业[{company_name}]股东信息失败!"
+        return f"获取企业[{company_name}]股东信息失败!{e}"
 
 @mcp.tool()
-async def get_stie_score(company_name:str) -> dict:
-    '''
+async def get_stie_score(company_name:str):
+    """
     评估企业科创能力，给出科创能力评分及等级
     :param company_name: 公司名称
     :return:
@@ -222,18 +241,18 @@ async def get_stie_score(company_name:str) -> dict:
     stii_quality_score_to_100 Number 创新质量评分（百分制）
     stii_influence_score_to_100 Number 创新影响评分（百分制）
     stii_develop_score_to_100 Number 创新成长评分（百分制）
-    '''
+    """
 
     try:
         return await ApiAdapter('https://api.shuidi.cn/utn/stie/score').invoke({'keyword': company_name})
     except Exception as e:
         logger.error(e)
-        return f"评估企业[{company_name}]科创能力失败！"
+        return f"评估企业[{company_name}]科创能力失败！{e}"
 
 
 @mcp.tool()
-async def search_company_risk(company_name:str) -> dict:
-    '''
+async def search_company_risk(company_name:str):
+    """
     根据公司名称查询公司的相关风险信息，包括企业自身风险、周边风险、预警信息等
     :param company_name: 公司名称
     :return:
@@ -250,18 +269,18 @@ async def search_company_risk(company_name:str) -> dict:
         事件类型包括：{"n1":"法定代表人变更","n2":"主要人员变更","n3":"股东信息变更","n4":"注册资本变更","n5":"对外投资变更","n6":"新增联系电话","n7":"新增联系邮箱","n8":"联系地址变更","n9":"新增icp备案",
                     "t1":"被执行人","t1h":"历史被执行人","t2":"失信被执行","t3":"裁判文书","t4":"法院公告","t5":"开庭公告","t6":"立案信息","t7":"终本案件","t8":"限制高消费","t9":"行政处罚(其它)","t10":" 欠税公告",
                     "t11":"税收违法","t12":"送达公告","t13":"司法拍卖","t14":"询价评估结果","t15":"询价评估机构","t16":"破产重整","t17":"司法协助","t18":"经营异常","t19":"行政处罚-工商","t20":"严重违法",
-                     "t21":"股权出质","t22":"清算信息","t23":"动产抵押","t24":"简易注销","t25":"知识产权出质","t26":"抽查检查","t27":"对外担保","c1":"裁判文书(合同纠纷)","c2":"开庭公告(合同纠纷)"}
+                    "t21":"股权出质","t22":"清算信息","t23":"动产抵押","t24":"简易注销","t25":"知识产权出质","t26":"抽查检查","t27":"对外担保","c1":"裁判文书(合同纠纷)","c2":"开庭公告(合同纠纷)"}
         company_events 中包含 cid 企业id, company_name 企业名称, company_event_count 该企业发生次数, tip 与查询企业的关系（1 表示股东，2 表示对外投资，3 表示分支机构, 空值表示当前查询企业）
-    '''
+    """
     try:
         return await ApiAdapter('https://api.shuidi.cn/utn/risk/CompanyRiskInfo').invoke({'keyword': company_name})
     except Exception as e:
         logger.error(e)
-        return f"获取企业[{company_name}]风险信息失败!"
+        return f"获取企业[{company_name}]风险信息失败!{e}"
 
 @mcp.tool()
-async def get_company_honor(company_name:str) -> dict:
-    '''
+async def get_company_honor(company_name:str):
+    """
     根据公司名称获取该公司的荣誉资质
     :param company_name: 公司名称
     :return:
@@ -273,17 +292,17 @@ async def get_company_honor(company_name:str) -> dict:
         PublishDate String 发布日期
         StartDate String 开始日期
         EndDate String 截止日期
-    '''
+    """
     try:
         return await ApiAdapter('https://api.shuidi.cn/utn/cf/Honor').invoke({'keyword': company_name})
     except Exception as e:
         logger.error(e)
-        return  f"获取企业[{company_name}]荣誉资质信息失败!"
+        return  f"获取企业[{company_name}]荣誉资质信息失败!{e}"
 
 
 @mcp.tool()
-async def get_company_contact(company_name:str) -> dict:
-    '''
+async def get_company_contact(company_name:str):
+    """
     根据公司名称获取该公司的联系方式，包括电话、邮箱、网站等
     :param company_name: 公司名称
     :return:
@@ -296,17 +315,17 @@ async def get_company_contact(company_name:str) -> dict:
         Website String 网站域名
         Title String 网站名称
         Icp String 网站备案号
-    '''
+    """
     try:
         return await ApiAdapter('https://api.shuidi.cn/utn/ic/GetContacts').invoke({'keyword': company_name})
     except Exception as e:
         logger.error(e)
-        return f"获取企业[{company_name}]联系方式失败!"
+        return f"获取企业[{company_name}]联系方式失败!{e}"
 
 
 @mcp.tool()
-async def get_company_investment(company_name:str) -> dict:
-    '''
+async def get_company_investment(company_name:str):
+    """
     根据企业名称获取该企业对外投资信息
     :param company_name: 企业名称
     :return:
@@ -320,17 +339,17 @@ async def get_company_investment(company_name:str) -> dict:
         CreditCode  String 社会统一信用代码
         EconKind String 企业类型
         FundedRatio String 出资比例
-    '''
+    """
     try:
         return await ApiAdapter('https://api.shuidi.cn/utn/ic/Invest/V3').invoke({'keyword': company_name})
     except Exception as e:
         logger.error(e)
-        return f"获取企业[{company_name}]对外投资信息失败!"
+        return f"获取企业[{company_name}]对外投资信息失败!{e}"
 
 
 @mcp.tool()
-async def get_company_cert(company_name:str) -> dict:
-    '''
+async def get_company_cert(company_name:str):
+    """
     根据企业名称获取该企业的资质证书
     :param company_name: 企业名称
     :return:
@@ -343,16 +362,16 @@ async def get_company_cert(company_name:str) -> dict:
         type    String  证书类型
         orgAn   String  发证单位
         status  String  证书状态
-    '''
+    """
     try:
         return await ApiAdapter('https://api.shuidi.cn/utn/ip/CertificateList/V2').invoke({'keyword': company_name})
     except Exception as e:
         logger.error(e)
-        return f"获取企业[{company_name}]资质证书失败!"
+        return f"获取企业[{company_name}]资质证书失败!{e}"
 
 @mcp.tool()
-async def get_person_related_company(company_name:str, person_name:str) -> dict:
-    '''
+async def get_person_related_company(company_name:str, person_name:str):
+    """
     通过公司名称和人名获取企业人员的所有相关公司，包括其担任法人、股东、董监高的公司信息
     :param company_name: 公司名称
     :param person_name: 人名
@@ -360,23 +379,23 @@ async def get_person_related_company(company_name:str, person_name:str) -> dict:
     total Number 总数
     isAll Number 是否已全部返回。1全部返回，0已返回前1000个企业
     items List  任职相关信息，每条信息包括：
-        estiblishTime Number 开业时间
+        establishTime Number 开业时间
         regStatus String 经营状态
         type String 类型，包括法人、股东、执行董事、高管等
         regCapital String 注册资本
         name String 公司名
         creditNo    String 统一信用代码
-    '''
+    """
     try:
         return await ApiAdapter('https://api.shuidi.cn/utn/cp/AllCompanys').invoke({'name': company_name, 'humanName':person_name})
     except Exception as e:
         logger.error(e)
-        return f"获取[{company_name}:{person_name}]的关联公司信息失败!"
+        return f"获取[{company_name}:{person_name}]的关联公司信息失败!{e}"
 
 
 @mcp.tool()
-async def get_company_controller(company_name:str) -> dict:
-    '''
+async def get_company_controller(company_name:str):
+    """
     根据企业名称获取该企业的实控人信息,并显示控制路径
     :param company_name: 企业名称
     :return:
@@ -395,16 +414,16 @@ async def get_company_controller(company_name:str) -> dict:
             Sequence Integer 路径序号。从0开始，依次向下穿透
             Proportion  String  持股比例（百分比）
             StartNode String 起始节点id
-    '''
+    """
     try:
         return await ApiAdapter('https://api.shuidi.cn/utn/pic/ActualController').invoke({'keyword': company_name})
     except Exception as e:
         logger.error(e)
-        return f"获取企业[{company_name}]的实控人信息失败!"
+        return f"获取企业[{company_name}]的实控人信息失败!{e}"
 
 @mcp.tool()
-async def get_company_benificalowner(company_name:str) -> dict:
-    '''
+async def get_company_benificalowner(company_name:str):
+    """
     根据企业名称查询该企业的受益所有人信息
     :param company_name: 企业名称
     :return:
@@ -425,12 +444,13 @@ async def get_company_benificalowner(company_name:str) -> dict:
             Type    String 关系类型。 INVEST 投资关系，OWN法定代表人关系，BRANCH 分总公司
             Sequence Integer 路径序号。从0开始，依次向下穿透
             Proportion String 持股比例
-    '''
+    """
     try:
         return await ApiAdapter('https://api.shuidi.cn/utn/pic/BeneficialOwner').invoke({'keyword': company_name})
     except Exception as e:
         logger.error(e)
-        return f"获取企业[{company_name}]受益所有人信息失败!"
+        return f"获取企业[{company_name}]受益所有人信息失败!{e}"
+
 
 def main():
     mcp.run(transport='stdio')
